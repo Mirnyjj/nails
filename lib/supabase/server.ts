8255cf9ffa6156aed445fd9ad09c6d25f0ab9b84
Mirnyjj <maks1733@gmail.com>
2026-01-20 14:13:35 +0400

    исправлены ошибки

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -2,20 +2,12 @@ import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 import { Database } from "./database.types";
 
-export async function createServerSupabaseClient() {
-  const cookieStore = await cookies();
-
+export const createServerSupabaseClient = () => {
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
-        },
-        set() {},
-        remove() {},
-      },
-    }
+      cookies: () => cookies(),
+    },
   );
-}
+};
